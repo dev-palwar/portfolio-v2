ac60a12abbe9ea9ee5816b6c435bc3e2d838e3e0
dev-palwar <devpalwar06@gmail.com>
2025-07-14 20:09:25 +0530

    added some animations and data api for exp section

--- src/Pages/exp/Exp.js
+++ src/Pages/exp/Exp.js
@@ -1,40 +1,45 @@
+import { useEffect, useState } from "react";
 import "./style.scss";
 
 const Experience = () => {
-  const experiences = [
-    {
-      title: "Frontend Developer Intern",
-      company: "Lumara jewels",
-      duration: "Dec 2023 - May 2024",
-      responsibilities: [
-        "Translated Figma designs into a fully functional homepage using Sass",
-        "Implemented a backend-driven UI.",
-        "Implemented authentication mechanisms, including manual and OTP-based login.",
-        "Streamlined the flow from the product details page to the cart page.",
-        "Developed a comprehensive product details page, fetching data from the backend and integrating components like carousels, image galleries, and product customization options for jewelry according to their metals, diamonds, and size.",
-        "Created a dynamic cart page with three subpages: cart details, address selection, and checkout, ensuring a smooth checkout process",
-      ],
-      link: "https://lumarajewels.com/",
-    },
-  ];
+  const [experiences, setExperiences] = useState([]);
+
+  useEffect(() => {
+    const fetchExperience = async () => {
+      try {
+        const response = await fetch(
+          "https://gist.githubusercontent.com/dev-palwar/78fc48a3253bd4ab5b58c9fb0b6e3235/raw/a09513e3fa1bf35115950552be7f63dcd4d1a361/gistfile1.txt"
+        );
+        const data = await response.json();
+        setExperiences(data); // wrap single object in array
+      } catch (error) {
+        console.error("Failed to load experience data", error);
+      }
+    };
+
+    fetchExperience();
+  }, []);
 
   return (
     <section className="experience">
-      {/* Current Status Section */}
-      <div className="experience__current-status">
+      {/* <div className="experience__current-status">
         <p className="experience__status-text">
           After completing my internship at Lumara Jewels, I took a career break
           to focus on higher studies. <br /> I am currently pursuing a Master's
-          degree (2024-2026) at IPU, Delhi.
+          degree (2024–2026) at IPU, Delhi.
         </p>
         <h3 className="experience__status-title">
           Current Status – Juggling clg and freelancing
         </h3>
-      </div>
-      {/* <Heading Heading={"Experience"} /> */}
+      </div> */}
+
       <div className="experience__list">
         {experiences.map((exp, index) => (
-          <div key={index} className="experience__card">
+          <div
+            key={index}
+            className="experience__card"
+            style={{ "--i": index }}
+          >
             <div className="experience__card-content">
               <h3 className="experience__card-title">{exp.title}</h3>
               <a href={exp.link} target="_blank" rel="noreferrer">
